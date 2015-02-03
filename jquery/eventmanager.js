ae2e924b4a3619cabcd1880c4f0f4e90205793d4
Martin Lonský <martin.lonsky@sabanero.cz>
2015-02-03 15:41:41 +0100

    add priority list

--- jquery/eventmanager.js
+++ jquery/eventmanager.js
@@ -14,17 +14,20 @@ var EventManager = function(){
 	 * @param bool once
 	 * @param string|array tag
 	 */
-	this.attach = function(event, callback, once, tag){
+	this.attach = function(event, callback, once, tag, priority){
 		if(typeof(listeners[event]) == 'undefined'){
 			listeners[event] =
-				[
-				];
+				[];
 		}
 		listeners[event][listeners[event].length] = {
 			callback : callback,
 			once     : typeof(once) != 'undefined' ? once : false,
-			tag      : typeof(tag) != 'undefined' ? tag : false
+			tag      : typeof(tag) != 'undefined' ? tag : false,
+			priority : typeof(tag) != 'undefined' ? priority : 0
 		};
+		listeners[event].sort(function(a, b){
+			return a.priority - b.priority;
+		});
 	};
 
 
@@ -45,13 +48,11 @@ var EventManager = function(){
 	this.trigger = function(event){
 		if(typeof(listeners[event]) != 'undefined'){
 			var detach =
-				[
-				];
+				[];
 			for(var item in listeners[event]){
 				try {
 					var args =
-						[
-						];
+						[];
 					for(var arg in arguments){
 						if(arg == 0){
 							continue;
@@ -83,12 +84,10 @@ var EventManager = function(){
 	 */
 	this.detachAllOnce = function(preg){
 		eventsdel =
-			[
-			];
+			[];
 		for(var event in listeners){
 			var detach =
-				[
-				];
+				[];
 			for(var o in listeners[event]){
 				if(listeners[event][o].once){
 					if(typeof(preg) == 'undefined'){
@@ -119,12 +118,10 @@ var EventManager = function(){
 	 */
 	this.detachAllOnceByTag = function(tag){
 		eventsdel =
-			[
-			];
+			[];
 		for(var event in listeners){
 			var detach =
-				[
-				];
+				[];
 			for(var o in listeners[event]){
 				if(listeners[event][o].once){
 					if(typeof(listeners[event][o].tag) == 'string'){
@@ -173,12 +170,10 @@ var EventManager = function(){
 	 */
 	this.detachByTag = function(tag){
 		eventsdel =
-			[
-			];
+			[];
 		for(var event in listeners){
 			var detach =
-				[
-				];
+				[];
 			for(var o in listeners[event]){
 				if(typeof(listeners[event][o].tag) == 'string'){
 					if(listeners[event][o].tag == tag){
